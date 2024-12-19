ec3bc401eb403e24dc27fb0e288ea786eebd04f2
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-12-19 09:41:04 -0800

    disable all processes for states

--- Scripts/State.cs
+++ Scripts/State.cs
@@ -5,9 +5,16 @@ namespace JumpHero
 	public abstract partial class State : Node
 	{
 		protected Player player;
+		protected PlayerStateManager stateManager;
 		public override void _Ready() 
 		{
 			player = GetOwner() as Player;
+			stateManager = GetParent() as PlayerStateManager;
+
+			// Disable to let state manager do the processing for states
+			SetProcessInput(false);
+			SetProcess(false);
+			SetPhysicsProcess(false);
 		}
 		public abstract void EnterState();
 		public abstract void ExitState();
