81299c8c6841a3aa7dc69741bc95f37a44ebce3a
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-12-19 09:38:54 -0800

    add pause input

--- Scripts/ProjectInputs.cs
+++ Scripts/ProjectInputs.cs
@@ -1,11 +1,12 @@
 namespace JumpHero
 {
-	public record class ProjectInputs
+	public static class ProjectInputs
 	{
 		public static readonly string JUMP = "Jump";
 		public static readonly string LEFT = "Left";
 		public static readonly string RIGHT = "Right";
 		public static readonly string UP = "Up";
 		public static readonly string DOWN = "Down";
+		public static readonly string PAUSE = "Pause";
 	}
 }
