6728f5ac248d64e6eac2734b4199e1462a55f8f3
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-12-19 09:40:39 -0800

    add data members for physics processing

--- Scripts/PlayerScripts/Player.cs
+++ Scripts/PlayerScripts/Player.cs
@@ -1,15 +1,39 @@
 using Godot;
+using JumpHero;
 using System;
 
-public partial class Player : RigidBody2D
+namespace JumpHero
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public partial class Player : CharacterBody2D
 	{
-	}
+		// static constants
+		public static readonly float DEFAULT_GRAVITY = 9.8f;
+		public static readonly float FREEFALL_THRESHOLD = 100f;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		// data members
+		public float Gravity { get; private set; } = DEFAULT_GRAVITY;
+		public float MoveSpeed { get; private set; } = 150f;
+		public bool IsFacingRight { get; private set; } = false;
+
+		// signals
+		[Signal] public delegate void OnStateChangeEventHandler();
+		[Signal] public delegate void OnDirectionChangeEventHandler();
+
+		public override void _Ready()
+		{
+
+		}
+
+		public void NotifyStateChange(PlayerStateManager.PlayerState oldState, PlayerStateManager.PlayerState newState)
+		{
+			EmitSignal(SignalName.OnStateChange, (int) oldState, (int) newState);
+		}
+
+		public void SetDirection(bool isRight)
+		{
+			if (IsFacingRight == isRight) return;
+			IsFacingRight = isRight;
+			EmitSignal(SignalName.OnDirectionChange, isRight);
+		}
 	}
 }
