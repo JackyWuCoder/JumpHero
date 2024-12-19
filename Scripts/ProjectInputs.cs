5440a9ae66d100a0d76c809fcf0470572fadfea7
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-12-18 19:32:00 -0800

    add inputs to project

---
+++ Scripts/ProjectInputs.cs
@@ -0,0 +1,11 @@
+namespace JumpHero
+{
+	public record class ProjectInputs
+	{
+		public static readonly string JUMP = "Jump";
+		public static readonly string LEFT = "Left";
+		public static readonly string RIGHT = "Right";
+		public static readonly string UP = "Up";
+		public static readonly string DOWN = "Down";
+	}
+}
