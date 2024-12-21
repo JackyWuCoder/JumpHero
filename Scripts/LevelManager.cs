f55f1495d08c9ee10db5d64bc6366ecfa9d233d9
Jacky Wu <JackyWuCoder@gmail.com>
2024-12-20 16:20:56 -0800

    fixed camera to level

--- Scripts/LevelManager.cs
+++ Scripts/LevelManager.cs
@@ -6,14 +6,17 @@ public partial class LevelManager : Node
 {
     public Camera2D Camera { get; private set; }
 
+    private float _cameraYOffset = -1296f; // Viewport Dimensions (1152 x 648) --> Camera Dimensions (2304 x 1296)
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        Camera = GetNode<Camera2D>("../Player/Camera2D");
+        Camera = GetNode<Camera2D>("../Camera2D");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+       // Camera.Position = new Vector2(Camera.Position.X, )
 	}
 }
