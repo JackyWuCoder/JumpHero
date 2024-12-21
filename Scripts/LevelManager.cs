0d0836469110d1e8d33a8b67dd3956d7b804f6ee
Jacky Wu <JackyWuCoder@gmail.com>
2024-12-20 16:58:46 -0800

    player transition to next level
    1) Need to add player transition back to previous level.

--- Scripts/LevelManager.cs
+++ Scripts/LevelManager.cs
@@ -5,7 +5,7 @@ using System;
 public partial class LevelManager : Node
 {
     public Camera2D Camera { get; private set; }
-    public Player player { get; private set; }
+    public Player Player { get; private set; }
 
     private float _cameraYOffset = -1296f; // Viewport Dimensions (1152 x 648) --> Camera Dimensions (2304 x 1296)
     private bool _isTransitioning = false;
@@ -13,19 +13,20 @@ public partial class LevelManager : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        player = GetNode<CharacterBody2D>("../Player") as Player;
+        Player = GetNode<CharacterBody2D>("../Player") as Player;
         Camera = GetNode<Camera2D>("../Camera2D");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-        if (player.Position.Y > (Camera.Position.Y + _cameraYOffset / 2)) {
+        if ((Player.Position.Y < (Camera.Position.Y + _cameraYOffset / 2)) && !_isTransitioning) {
             StartLevelTransition();
         }
         if (_isTransitioning)
         {
             Camera.Position = new Vector2(Camera.Position.X, Camera.Position.Y + _cameraYOffset);
+            _isTransitioning = false;
         }
 	}
 
