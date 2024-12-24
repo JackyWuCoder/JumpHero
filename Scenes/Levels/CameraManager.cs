639b3ed6c88365debb028fdbced3c329ab09dea5
Jacky Wu <JackyWuCoder@gmail.com>
2024-12-24 15:28:35 -0800

    Connect OnLevelTransitioned signal to the OnLevelTransitioned event handler

---
+++ Scenes/Levels/CameraManager.cs
@@ -0,0 +1,27 @@
+using Godot;
+using JumpHero;
+using System;
+
+public partial class CameraManager : Camera2D
+{
+    private bool _isTransitioning = false;
+    private Vector2 _targetPosition;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        var levelManager = GetParent() as LevelManager;
+        levelManager.Connect(LevelManager.SignalName.OnLevelTransitioned, Callable.From((Vector2 newCameraPosition) => OnLevelTransitioned(newCameraPosition)) );
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+    private void OnLevelTransitioned(Vector2 newCameraPosition)
+    {
+
+    }
+
+}
