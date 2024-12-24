9660fdeee27964193a5eb63cb45c136ae9d27173
Jacky Wu <JackyWuCoder@gmail.com>
2024-12-24 15:23:28 -0800

    fixed player and camera node paths

--- Scripts/LevelManager.cs
+++ Scripts/LevelManager.cs
@@ -14,8 +14,8 @@ public partial class LevelManager : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        Player = GetNode<CharacterBody2D>("../Player") as Player;
-        Camera = GetNode<Camera2D>("../Camera2D");
+        Player = GetNode<CharacterBody2D>("Player") as Player;
+        Camera = GetNode<Camera2D>("Camera2D");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
