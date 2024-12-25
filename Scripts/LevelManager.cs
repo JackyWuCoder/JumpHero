47ea52fd26dcd6edb6d7bac7a2ebf23d4b1b3c00
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-12-25 04:31:15 -0800

    move camera position calculation into camera manager class

--- Scripts/LevelManager.cs
+++ Scripts/LevelManager.cs
@@ -4,40 +4,29 @@ using System;
 
 public partial class LevelManager : Node
 {
-    public Player Player { get; private set; }
-    public Camera2D Camera { get; private set; }
+    // Node names
+    private static readonly string PLAYER_NODE_NAME = "Player";
 
-    [Signal] public delegate void OnLevelTransitionedEventHandler(Vector2 newCameraPosition);
+    // Data members
+    private Player _player;
 
-    private float _cameraYOffset = -1296f; // Viewport Dimensions (1152 x 648) --> Camera Dimensions (2304 x 1296)
+    // Signals
+    [Signal] public delegate void LevelTransitionEventHandler(Player player);
+    [Signal] public delegate void WorldTransitionEventHandler(Player player);
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        Player = GetNode<CharacterBody2D>("Player") as Player;
-        Camera = GetNode<Camera2D>("Camera2D");
+        _player = GetNode<Player>(PLAYER_NODE_NAME);
+        _player.Connect(Player.SignalName.OnScreenExit,
+            Callable.From(() => {
+                CheckNewWorld();
+                EmitSignal(SignalName.LevelTransition, _player);
+            }
+        ));
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-        PlayerLevelTransition();
-	}
-
-    private void PlayerLevelTransition()
+    private void CheckNewWorld()
     {
-        if (Player.Position.Y < (Camera.Position.Y + _cameraYOffset / 2))
-        {
-            RequestLevelTransition(Camera.Position + new Vector2(0, _cameraYOffset));
-        }
-        else if (Player.Position.Y > (Camera.Position.Y - _cameraYOffset / 2))
-        {
-            RequestLevelTransition(Camera.Position - new Vector2(0, _cameraYOffset));
-        }
-    }
-
-    private void RequestLevelTransition(Vector2 newCameraPosition)
-    {
-        EmitSignal(SignalName.OnLevelTransitioned, newCameraPosition);
+        // TODO: Implement when more levels are finished, checks if 5 levels are passed
     }
 }
