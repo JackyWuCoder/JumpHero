f622b923b43d374d00610c62db6aa9f5066121b7
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-12-31 06:00:32 -0800

    add toggle to if button needs to be enable on ready

--- Scripts/MenuScripts/MenuEffects/ButtonSnap.cs
+++ Scripts/MenuScripts/MenuEffects/ButtonSnap.cs
@@ -3,9 +3,11 @@ using System;
 
 public partial class ButtonSnap : Button
 {
+	[Export] private bool _activeOnReady = true;
 	[Export] private float _hoveredScale = 1.3f;
 	[Export] private float _hoveredAngle = -20f;
 	[Export] private float _snapSpeed = 0.1f;
+	private bool _active;
 	private float _hoverAngleInRad = 0;
 	public override void _Ready()
 	{
@@ -13,9 +15,16 @@ public partial class ButtonSnap : Button
 		_hoverAngleInRad = Mathf.DegToRad(_hoveredAngle);
 		Connect(SignalName.MouseEntered, Callable.From(PlayHoverEffect));
 		Connect(SignalName.MouseExited, Callable.From(RevertHoverEffect));
+		
+		_active = _activeOnReady;
+		// Used to tell button when button effects can be played, we do this to avoid casting by user
+		// Assumes theme used for buttons and UI will never change
+		if (!_activeOnReady) Connect(SignalName.ThemeChanged, Callable.From(() => _active = true));
 	}
 	private void PlayHoverEffect()
 	{
+		if (!_active) return; // Don't play button effect if not active yet
+
 		Tween scaleTween = CreateTween();
 		Tween angleTween = CreateTween();
 		scaleTween.TweenProperty(this, nameof(Scale).ToLower(), Vector2.One * _hoveredScale, _snapSpeed);
@@ -25,6 +34,8 @@ public partial class ButtonSnap : Button
 	}
 	private void RevertHoverEffect()
 	{
+		if (!_active) return;
+
 		Tween scaleTween = CreateTween();
 		Tween angleTween = CreateTween();
 		scaleTween.TweenProperty(this, nameof(Scale).ToLower(), Vector2.One, _snapSpeed);
